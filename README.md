f5067f56035b443616f370d8d28595b8f9b030c9
Nacho Caiafa <nachokai@outlook.com>
2020-03-24 18:39:07 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+# Fake News Check
+
+Front para el proyecto del Hackatón https://lohackeamosentretodos.com/ 
+
+**Demo:** https://check-fakes.herokuapp.com/  
+  
+##Herramientas  
+•HTML  
+•CSS (Scss)  
+•Bootstrap  
+•Javascript  
+•Nodejs/Express  
+•Heroku  
---
+++ public/src/main.js
@@ -0,0 +1,25 @@
+let checkear = document.querySelector('#checkear');
+let checkeo = document.querySelector('#checkeo');
+
+checkear.addEventListener('click', clickCheck);
+checkeo.addEventListener('click', clickCheckBar);
+
+function clickCheck() {
+	randomize();
+	checkeo.className = 'visible'
+}
+
+function clickCheckBar() {
+	randomize();
+	checkeo.className = 'oculto';
+}
+
+function randomize() {
+	$('.progressBar').each(function (index, progressBar) {
+		let percentage = $(progressBar).find('.percentage-check');
+		let progress = $(progressBar).find('.progress-check');
+		randNum = Math.floor(Math.random() * 100);
+		$(percentage).text(randNum + '%');
+		$(progress).width(randNum + '%');
+	});
+}
